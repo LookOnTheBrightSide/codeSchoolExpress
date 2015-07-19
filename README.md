84668e7badd0823762c519c1a676b85d2433a14a
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-07-19 22:50:06 +0200

    learnt a trick about curl

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Building blocks course 
 This is a course I am taking at codeschool
 I have been learning Express for a while now but I feel like I need to go over the basics again...
+### I am quite surprised how thorough the codeschool course it!
--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/locations', function (request, response) {
-  var cities = ['Caspiana', 'Indigo', 'Paradise'];
-  response.send(cities);
+app.get('/locations', function(request, response) {
+    var cities = ['Caspiana', 'Indigo', 'Paradise'];
+    response.send(cities);
+});
+app.get('/locals', function(request, response) {
+    var cities = '<ul><li>Caspiana</li><li>Indigo</li></ul>';
+    response.send(cities);
+});
+app.get('/location', function (req,res){
+	//permanent redirect
+	res.redirect(301,'/locals');
 });
 
-app.listen(3001, function () {
-  console.log("Running Express");
+app.listen(3001, function() {
+    console.log("Running Express");
 });
+//using curl -i localhost:3001 to check out my headers
